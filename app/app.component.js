039d9a9cbb1262475718e46c44e3bfcd032c48d0
aldric <gaudinot@gmail.com>
2016-11-08 22:08:40 +0100

    add list

--- app/app.component.js
+++ app/app.component.js
@@ -8,18 +8,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
+var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Bank Comparer';
+        this.banks = BANKS;
     }
-    AppComponent = __decorate([
-        core_1.Component({
-            selector: 'my-app',
-            template: '<h1>My First Angular App</h1>'
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppComponent);
+    AppComponent.prototype.onSelect = function (bank) {
+        this.selectedBank = bank;
+    };
     return AppComponent;
 }());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: "\n  <h1>{{title}}</h1>\n  <h2>My Banks</h2>\n  <ul class=\"banks\">\n    <li *ngFor=\"let bank of banks\" (click)=\"onSelect(bank)\" [class.selected]=\"bank === selectedBank\">\n      <span class=\"badge\">{{bank.id}}</span> {{bank.name}}\n    </li>\n  </ul>\n  <div *ngIf=\"selectedBank\">\n    <h2>{{selectedBank.name}} details!</h2>\n    <div><label>id: </label>{{selectedBank.id}}</div>\n    <div><label>name: </label><input [(ngModel)]=\"selectedBank.name\" placeholder=\"name\"></div>\n  </div>\n  ",
+        styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .banks {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .banks li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .banks li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .banks li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .banks .text {\n    position: relative;\n    top: -3px;\n  }\n  .banks .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
+    }),
+    __metadata("design:paramtypes", [])
+], AppComponent);
 exports.AppComponent = AppComponent;
+var Bank = (function () {
+    function Bank() {
+    }
+    return Bank;
+}());
+exports.Bank = Bank;
+var BANKS = [
+    { id: 11, name: 'BforBank' },
+    { id: 12, name: 'Monabanq' },
+    { id: 13, name: 'ING Direct' },
+    { id: 14, name: 'Boursorama' }
+];
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,88 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: `
+  <h1>{{title}}</h1>
+  <h2>My Banks</h2>
+  <ul class="banks">
+    <li *ngFor="let bank of banks" (click)="onSelect(bank)" [class.selected]="bank === selectedBank">
+      <span class="badge">{{bank.id}}</span> {{bank.name}}
+    </li>
+  </ul>
+  <div *ngIf="selectedBank">
+    <h2>{{selectedBank.name}} details!</h2>
+    <div><label>id: </label>{{selectedBank.id}}</div>
+    <div><label>name: </label><input [(ngModel)]="selectedBank.name" placeholder="name"></div>
+  </div>
+  `,
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .banks {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .banks li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .banks li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .banks li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .banks .text {
+    position: relative;
+    top: -3px;
+  }
+  .banks .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Bank Comparer';
+  selectedBank : Bank;
+  banks = BANKS;
+
+  onSelect(bank: Bank): void {
+    this.selectedBank = bank;
+  }
+}
+
+ export class Bank {
+   id : number;
+   name : string;
+ }
+
+ const BANKS: Bank[] = [
+  { id: 11, name: 'BforBank' },
+  { id: 12, name: 'Monabanq' },
+  { id: 13, name: 'ING Direct' },
+  { id: 14, name: 'Boursorama' }
+];
